c70faec66ac992b356e0c0c0b49e05a2c651f275
yann.mallegol@isen-bretagne.fr <rudolph.pienaar@gmail.com>
2015-08-17 16:23:58 -0400

    Django test 1

---
+++ tutorial/polls/urls.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 11:01:43 2015
+
+@author: yann.mallegol
+"""
+from django.conf.urls import url
+
+from . import views
+
+	urlpatterns = [
+    # ex: /polls/
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+    ]
