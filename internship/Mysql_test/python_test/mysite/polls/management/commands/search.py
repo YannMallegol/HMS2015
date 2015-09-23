cda93e739b7a6e874e64b03a37fdb8d2ce2779ed
yann.mallegol@isen-bretagne.fr <rudolph.pienaar@gmail.com>
2015-09-23 10:13:50 -0400

    search

--- internship/Mysql_test/python_test/mysite/polls/management/commands/search.py
+++ internship/Mysql_test/python_test/mysite/polls/management/commands/search.py
@@ -27,41 +27,69 @@ class Command(BaseCommand):
 
         print(' ')
         
-    def target_table(query_string): 
+    '''def target_table(self,query_string): 
 
-        query_string=re.compile(r'[^\s";,.:]+').findall(query_string)
-        terms = query_string   
+        #query_string=re.compile(r'[^\s";,.:]+').findall(query_string)
+        new_query=query_string.split()
+        terms = new_query
 
-        for term in terms:
+        #for term in terms:
         
-            if term == "PatientAge" or term == "PatientSex"or term == "PatientID" or term == "PatientName" or term == "PatientPatientBirthDate" or term == "PatientPatientBirthTime":
+        if term[0] == "PatientAge" or term[0] == "PatientSex"or term[0] == "PatientID" or term[0] == "PatientName" or term[0] == "PatientPatientBirthDate" or term[0] == "PatientPatientBirthTime":
                 
-                table= "Patient"
-                search_fields= ['PatientID','PatientName','PatientSex','PatientAge', 'PatientBirthDate', 'PatientBirthTime']
+            table= "Patient"
+            search_fields= ['PatientID','PatientName','PatientSex','PatientAge', 'PatientBirthDate', 'PatientBirthTime']
+
                 
 
-            elif term == "StudyDescription" or term == "StationName" or term == "ManufacturerModelName" or term == "StudyInstanceUID" or term == "Pathology" or term == "StudyDate" or term == "StudyTime" or term == "InstitutionName" or term == "ReferringPhysicianName" or term == "PerformingPhysicianName"or term == "ModalitiesInStudy" or term == "MagneticFieldStrength":
-                table="Study"
-                search_fields=['StudyDescription','StationName','ManufacturerModelName','StudyInstanceUID','Pathology','StudyDate','StudyTime','InstitutionName','ReferringPhysicianName','PerformingPhysicianName','ModalitiesInStudy','MagneticFieldStrength']
+        elif term[0] == "StudyDescription" or term[0] == "StationName" or term[0] == "ManufacturerModelName" or term[0] == "StudyInstanceUID" or term[0] == "Pathology" or term[0] == "StudyDate" or term[0] == "StudyTime" or term[0] == "InstitutionName" or term[0] == "ReferringPhysicianName" or term[0] == "PerformingPhysicianName"or term[0] == "ModalitiesInStudy" or term[0] == "MagneticFieldStrength":
+            table="Study"
+            search_fields=['StudyDescription','StationName','ManufacturerModelName','StudyInstanceUID','Pathology','StudyDate','StudyTime','InstitutionName','ReferringPhysicianName','PerformingPhysicianName','ModalitiesInStudy','MagneticFieldStrength']
                 
-            elif term=="+":
-                table=
-            else:
-                table="Series"                  
+           
+        else:
+            table="Series"  
+            search_fields=['SeriesDescription','SeriesInstanceUID']                
 
-        return(table, search_fields)  
+        return(table, search_fields)'''
 
 
 
-    def get_query(query_string):  
+    def get_query(query_string,search_fields):  
         
         query=None 
-        query_string=re.compile(r'[^\s";,.:]+').findall(query_string)
-        terms = query_string
-        print(terms)
-        choice_table = target_table(query_string)
+        #query_string=re.compile(r'[^\s";,.:]+').findall(query_string)
+        new_query=query_string.split()
+        dimension_query = len(new_query)
+        term=new_query
 
-        for term in terms:
+        if term[0] == "PatientAge" or term[0] == "PatientSex"or term[0] == "PatientID" or term[0] == "PatientName" or term[0] == "PatientPatientBirthDate" or term[0] == "PatientPatientBirthTime":
+                
+            table= "Patient"
+            table_patient=term[]
+            search_fields= ['PatientID','PatientName','PatientSex','PatientAge', 'PatientBirthDate', 'PatientBirthTime']
+
+                
+
+        elif term[0] == "StudyDescription" or term[0] == "StationName" or term[0] == "ManufacturerModelName" or term[0] == "StudyInstanceUID" or term[0] == "Pathology" or term[0] == "StudyDate" or term[0] == "StudyTime" or term[0] == "InstitutionName" or term[0] == "ReferringPhysicianName" or term[0] == "PerformingPhysicianName"or term[0] == "ModalitiesInStudy" or term[0] == "MagneticFieldStrength":
+            table="Study"
+            table_study=term[]
+            search_fields=['StudyDescription','StationName','ManufacturerModelName','StudyInstanceUID','Pathology','StudyDate','StudyTime','InstitutionName','ReferringPhysicianName','PerformingPhysicianName','ModalitiesInStudy','MagneticFieldStrength']
+                
+           
+        else:
+            table="Series"  
+            table_series=term[]
+            search_fields=['SeriesDescription','SeriesInstanceUID']  
+
+        #print(new_query)
+        print(dimension_query)
+        #terms = new_query
+        print(term)
+        print(table)
+        #self.target_table(query_string)
+        print(search_fields)
+        for each_term in term:
             or_query = None
             for field_name in search_fields:
                 q = Q(**{"%s__icontains" % field_name: term})
@@ -81,7 +109,7 @@ class Command(BaseCommand):
 
 
 
-    qry = get_query('  PatientSex M ')
+    qry = get_query(' PatientSex : 14001 M ',['PatientSex'])
 
     
 
